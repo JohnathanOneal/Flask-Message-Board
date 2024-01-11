df03eef107bc3e712aec39bf3e42887410ad5814
JohnathanOneal <johmathanoneal@gmail.com>
2024-01-10 19:46:59 -0500

    add sqlite database and cli command to init

--- board/__init__.py
+++ board/__init__.py
@@ -4,16 +4,27 @@
 #  parts of your application, like routes, configurations, and initializations, into different files later on.
 #  This encourages a cleaner and more maintainable codebase.
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 # pages is where we create our blueprint and defined the two routes
-from board import pages, posts
+from board import pages, posts, database
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
+    # When you use app.config.from_prefixed_env() you have access to all environment variables that start with FLASK_
+    app.config.from_prefixed_env()
+    # you can access them with app.config.get() with the variable named after this prefix. That’s why you pass DATABASE
+    # instead of FLASK_DATABASE into .get(). For any other environment variables that don’t start with FLASK_, you can use os.getenv().
+
+    database.init_app(app)
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
 
+    print(os.getenv('ENVIRONMENT'))
+    print(app.config.get('DATABASE'))
     return app
---
+++ board/database.py
@@ -0,0 +1,36 @@
+# The sqlite3 module, a built-in Python module for interacting with SQLite databases
+import sqlite3
+# click, a Python package for creating command-line interfaces (CLI)
+import click
+
+# The current_app object points to the Flask application handling the current activity. You can use it to access
+# application-specific data like your environment variables. The use of current_app makes your code more flexible
+from flask import current_app, g
+# With g, Flask provides you with a global namespace object that you can use as temporary storage when a user makes a
+# request, such as sending a form. Each request has its own g object, which resets at the end of the request. This is
+# useful for storing data that might be accessed multiple times during the request, such as a database connection,
+# avoiding the need to create new connections for every single database operation within the same request
+
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
+
+@click.command("init-db")
+def init_db_command():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
+
+    click.echo('Database Successfully Initialized')
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"],
+            detect_types=sqlite3.PARSE_DECLTYPES,
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
