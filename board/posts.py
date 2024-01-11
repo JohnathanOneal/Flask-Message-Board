350ddc1fb88895c79dd479fecccaa76918f95b4f
JohnathanOneal <johmathanoneal@gmail.com>
2024-01-11 15:59:43 -0500

    add logic to post and retrive from db add css styling

--- board/posts.py
+++ board/posts.py
@@ -1,14 +1,38 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+from board.database import get_db
 
 bp = Blueprint("posts",__name__)
 
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
+    if request.method == 'POST':
+        author = request.form['author'] or 'Anonymous spirit'
+        message = request.form['message']
+
+        if message:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (author,message) VALUES (?,?)',
+                (author, message),
+            )
+            db.commit()
+
     return render_template("posts/create.html")
 
 
 @bp.route("/posts")
 def posts():
-    posts = []
+    db = get_db()
+    posts = db.execute(
+        'SELECT author, message, created FROM post ORDER BY created DESC'
+    ).fetchall()
+
     return render_template("posts/posts.html",posts=posts)
